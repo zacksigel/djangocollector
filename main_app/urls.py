439fea0a2d2ad5b8eb0ccd3a65d2e05d7aef283e
Zack Sigel <zack.sigel@gmail.com>
2023-04-12 20:15:20 -0400

    adds details page

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path("about/", views.about, name="about"),
-  path("djangos/", views.djangos_index, name="index")
+  path("djangos/", views.djangos_index, name="index"),
+  path("djangos/<str:django_title>/", views.djangos_detail, name="detail")
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import djangos
+from .models import Film
 
 # Create your views here.
 def home(request):
-  return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+  return render(request, "about.html")
 
 def about(request):
   return render(request, "about.html")
 
 # Add new view
 def djangos_index(request):
+  djangos = Film.objects.all()
   return render(request, 'djangos/index.html', {'djangos': djangos})
+
+def djangos_detail(request, django_title):
+  django = Film.objects.get(title=django_title)
+  return render(request, "djangos/detail.html", {"django": django})
